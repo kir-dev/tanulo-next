93f39c7d3ff37056fad776b9e255767698231638
triszt4n <trisztanpiller@gmail.com>
2020-04-26 22:55:36 +0200

    Use flatpickr as DateTime picker #106 Implementing flatpickr's DateTime picker, in the non-module way. Handles input start and end dates, looks out for illegal intervals, like - Date in the past - Starting date later than ending date (and vice versa) --> solution: shifting ranges This way, the PR solves issue #206 as well.

--- public/js/picker.js
+++ public/js/picker.js
@@ -1,21 +1,59 @@
-//source: https://bossanova.uk/jsuites/javascript-calendar
+//Adding hours and minutes are be easier with these prototype methods:
+Date.prototype.addHours = function(h) {
+  const temp = new Date()
+  temp.setTime(this.getTime() + (h*60*60*1000))
+  return temp
+}
+Date.prototype.addMinutes = function(m) {
+  const temp = new Date()
+  temp.setTime(this.getTime() + (m*60*1000))
+  return temp
+}
 
-const pickerStart = jSuites.calendar(document.getElementById('pickerStart'), {
-  time: true,
-  format: 'YYYY-MM-DD HH24:MI',
-  resetButton: false,
-  startingDay: 1,
-  onclose: function() {
-    pickerStart.setValue(pickerStart.getValue())
+//Main logic:
+const now = new Date()
+now.setMinutes(0, 0, 0)
+
+let startDef = now.addHours(1)
+let endDef = now.addHours(2)
+
+if (typeof range !== 'undefined') {
+  const parsedStart = flatpickr.parseDate(range.start, 'Y-m-dTH:i')
+  if (parsedStart >= now) {
+    startDef = parsedStart
+    endDef = flatpickr.parseDate(range.end, 'Y-m-dTH:i')
+  }
+  else
+    alert('Hiba: múltbéli időintervallumban csoport nem hozható létre.')
+}
+
+const pickerStart = flatpickr('#pickerStart', {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+  minDate: 'today',
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  time_24hr: true,
+  defaultDate: startDef
+})
+
+const pickerEnd = flatpickr('#pickerEnd', {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+  minDate: 'today',
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  time_24hr: true,
+  defaultDate: endDef
+})
+
+//Watching out for illegal DateTime ranges between start and end:
+pickerStart.config.onChange.push(function() {
+  if (pickerStart.selectedDates[0] >= pickerEnd.selectedDates[0]) {
+    pickerEnd.setDate(pickerStart.selectedDates[0].addMinutes(5))
   }
 })
 
-const pickerEnd = jSuites.calendar(document.getElementById('pickerEnd'), {
-  time: true,
-  format: 'YYYY-MM-DD HH24:MI',
-  resetButton: false,
-  startingDay: 1,
-  onclose: function() {
-    pickerEnd.setValue(pickerEnd.getValue())
+pickerEnd.config.onChange.push(function() {
+  if (pickerStart.selectedDates[0] >= pickerEnd.selectedDates[0]) {
+    pickerStart.setDate(pickerEnd.selectedDates[0].addMinutes(-5))
   }
 })
