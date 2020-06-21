9962bf180c35ed6a48df076a70772410846b1166
János Fehér <feherjani96@gmail.com>
2020-06-21 16:09:29 +0200

    Validate group fields #142

--- src/components/groups/group.routes.ts
+++ src/components/groups/group.routes.ts
@@ -4,13 +4,51 @@ import {
   formatDistanceStrict
 } from 'date-fns'
 import huLocale from 'date-fns/locale/hu'
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
+import { check } from 'express-validator'
+import multer from 'multer'
 
 import { isAuthenticated } from '../../config/passport'
 import { DATE_FORMAT, ROOMS } from '../../util/constants'
+import { handeValidationError } from '../../util/errorHandlers'
 import { User } from '../users/user'
 import { isGroupOwner, joinGroup, leaveGroup } from './group.middlewares'
 import { createGroup, getGroup, getGroups, removeGroup } from './group.service'
+
+function validateGroup() {
+  return [
+    check('name', 'A csoport neve max 100 karakter hosszú nem üres szöveg lehet')
+      .isString()
+      .exists({ checkNull: true, checkFalsy: true })
+      .notEmpty()
+      .trim()
+      .isLength({ max: 100 }),
+    check('tags')
+      .optional({ nullable: true, checkFalsy: true })
+      .isString()
+      .custom((value: string) => value.split(',').length <= 8)
+      .withMessage('Max 8 címke adható hozzá')
+      .custom((value: string) => value.split(',').every(it => it.length <= 30))
+      .withMessage('A címkék egyenként max 30 karakter hosszúak lehetnek'),
+    check('room')
+      .exists({ checkNull: true })
+      .withMessage('A szint nem lehet üres')
+      .isInt({ gt: 2, lt: 19 })
+      .withMessage('A szint csak 3 és 18 közötti értéket vehet fel'),
+    check('startDate')
+      .exists({ checkNull: true, checkFalsy: true })
+      .isAfter()
+      .withMessage('Múltbéli kezdéssel csoport nem hozható létre')
+      .custom((value, { req }) => new Date(value).getTime() < new Date(req.body.endDate).getTime())
+      .withMessage('A kezdés nem lehet korábban, mint a befejezés'),
+    check('endDate', 'A befejezés időpontja kötelező')
+      .exists({ checkFalsy: true, checkNull: true }),
+    check('description', 'A leírás max 500 karakter lehet')
+      .optional({ nullable: true })
+      .isString()
+      .isLength({ max: 500 })
+  ]
+}
 
 const router = Router()
 
@@ -37,7 +75,15 @@ router.get('/new', isAuthenticated, (req, res) =>
   })
 )
 
-router.post('/', isAuthenticated, createGroup, joinGroup, (req, res) => res.redirect('/groups'))
+router.post('/',
+  isAuthenticated,
+  multer().none(),
+  validateGroup(),
+  handeValidationError(400),
+  createGroup,
+  joinGroup,
+  (req: Request, res: Response) => res.sendStatus(201)
+)
 
 router.get('/:id', isAuthenticated, getGroup, (req, res) => {
   const joined = req.group.users.some(u => u.id === (req.user as User).id)
