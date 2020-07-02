239cc66ad7ce079acb636e70f50744d3c7e8a573
János Fehér <feherjani96@gmail.com>
2020-07-02 16:40:59 +0200

    Check for overlapping start and end dates #130

--- src/components/groups/group.routes.ts
+++ src/components/groups/group.routes.ts
@@ -4,51 +4,22 @@ import {
   formatDistanceStrict
 } from 'date-fns'
 import huLocale from 'date-fns/locale/hu'
-import { Request, Response, Router } from 'express'
-import { check } from 'express-validator'
+import { Request, Response, Router} from 'express'
 import multer from 'multer'
 
 import { isAuthenticated } from '../../config/passport'
 import { DATE_FORMAT, ROOMS } from '../../util/constants'
 import { handleValidationError } from '../../util/errorHandlers'
 import { User } from '../users/user'
-import { isGroupOwner, joinGroup, leaveGroup, createICSEvent } from './group.middlewares'
+import {
+  isGroupOwner,
+  joinGroup,
+  leaveGroup,
+  createICSEvent,
+  checkConflicts,
+  validateGroup
+} from './group.middlewares'
 import { createGroup, getGroup, getGroups, removeGroup } from './group.service'
-
-function validateGroup() {
-  return [
-    check('name', 'A csoport neve max 100 karakter hosszú nem üres szöveg lehet')
-      .isString()
-      .exists({ checkNull: true, checkFalsy: true })
-      .notEmpty()
-      .trim()
-      .isLength({ max: 100 }),
-    check('tags')
-      .optional({ nullable: true, checkFalsy: true })
-      .isString()
-      .custom((value: string) => value.split(',').length <= 8)
-      .withMessage('Max 8 címke adható hozzá')
-      .custom((value: string) => value.split(',').every(it => it.length <= 30))
-      .withMessage('A címkék egyenként max 30 karakter hosszúak lehetnek'),
-    check('room')
-      .exists({ checkNull: true })
-      .withMessage('A szint nem lehet üres')
-      .isInt({ gt: 2, lt: 19 })
-      .withMessage('A szint csak 3 és 18 közötti értéket vehet fel'),
-    check('startDate')
-      .exists({ checkNull: true, checkFalsy: true })
-      .isAfter()
-      .withMessage('Múltbéli kezdéssel csoport nem hozható létre')
-      .custom((value, { req }) => new Date(value).getTime() < new Date(req.body.endDate).getTime())
-      .withMessage('A kezdés nem lehet korábban, mint a befejezés'),
-    check('endDate', 'A befejezés időpontja kötelező')
-      .exists({ checkFalsy: true, checkNull: true }),
-    check('description', 'A leírás max 500 karakter lehet')
-      .optional({ nullable: true })
-      .isString()
-      .isLength({ max: 500 })
-  ]
-}
 
 const router = Router()
 
@@ -80,6 +51,7 @@ router.post('/',
   multer().none(),
   validateGroup(),
   handleValidationError(400),
+  checkConflicts,
   createGroup,
   joinGroup,
   (req: Request, res: Response) => res.sendStatus(201)
