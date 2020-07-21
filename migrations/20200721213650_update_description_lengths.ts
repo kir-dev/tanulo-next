1fb3895cd95e77ce27d1a2a94fa7fd39fb21f618
János Fehér <feherjani96@gmail.com>
2020-07-21 21:46:36 +0200

    Fix description length in the database #375

---
+++ migrations/20200721213650_update_description_lengths.ts
@@ -0,0 +1,28 @@
+import * as Knex from 'knex'
+
+export async function up(knex: Knex): Promise<void[]> {
+  return Promise.all(
+    [
+      knex.schema.alterTable('groups', table => {
+        table.string('description', 500).alter()
+      }),
+      knex.schema.alterTable('tickets', table => {
+        table.string('description', 500).alter()
+      })
+    ]
+  )
+}
+
+export async function down(knex: Knex): Promise<void[]> {
+  return Promise.all(
+    [
+      knex.schema.alterTable('groups', table => {
+        table.string('description').alter()
+      }),
+      knex.schema.alterTable('tickets', table => {
+        table.string('description').alter()
+      })
+    ]
+  )
+}
+
