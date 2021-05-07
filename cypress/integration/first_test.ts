8f623e2d4b866ddbe1fcf86cc1e1c6f47c5d89f6
Tschonti <feketesamu@gmail.com>
2021-05-07 15:11:44 +0200

    calendar test

--- cypress/integration/first_test.ts
+++ cypress/integration/first_test.ts
@@ -1,9 +1,20 @@
 describe('My First Test', () => {
   it('Visits TanuloSCH', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
 
     cy.contains('Naptár').click()
 
     cy.url().should('include', '/rooms/3')
+
+    const today = new Date()
+
+    cy.get('.fc-today').find('span')
+      .should('include.text', `${today.getMonth() + 1}. ${today.getDate()}.`)
+
+    cy.get('.fc-icon-chevron-right').click()
+    cy.contains('ma').click()
+
+    //cy.get('.fc-center').find('h2').should('have.text', )
+
   })
 })
