a26b4670629d8f404fd90bbafb0238914debf3f9
Tschonti <feketesamu@gmail.com>
2021-05-07 15:18:55 +0200

    calendar test, with old calendar classnames'

--- cypress/integration/first_test.ts
+++ cypress/integration/first_test.ts
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-  it('Visits TanuloSCH', () => {
+describe('Calendar page test', () => {
+  it('interacts with the calendar', () => {
     cy.visit('/')
 
     cy.contains('Naptár').click()
@@ -14,7 +14,8 @@ describe('My First Test', () => {
     cy.get('.fc-icon-chevron-right').click()
     cy.contains('ma').click()
 
-    //cy.get('.fc-center').find('h2').should('have.text', )
+    cy.get('.fc-today').find('span')
+      .should('include.text', `${today.getMonth() + 1}. ${today.getDate()}.`)
 
   })
 })
