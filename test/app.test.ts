77ddd8051bddeff8cb932d9e1da63590c20d06b7
Sámuel Fekete <feketesamu@gmail.com>
2021-05-25 00:07:58 +0200

    Automatically test new pushes with Cypress through Github Actions (#688)
    * removed jest from the project
    
    * added cypress.yml for Github Actions
    
    * jest removed from more places
    
    * trying to make cypress work in CI
    
    * trying to make cypress work in CI
    
    * trying to make cypress work in CI
    
    * trying to make cypress work in CI
    
    * trying to make cypress work in CI
    
    * trying to make cypress work in CI
    
    * Delete package-lock.json
    
    * attempt to run tests with sqlite db
    
    * Revert "trying to make cypress work in CI"
    
    This reverts commit 4ea29fe2c2369e89939c6ad760e28b12f1f4bd6a.
    
    * back to postgres
    
    * postgres server in the workflow
    
    * postgres server in the workflow
    
    * postgres server in the workflow
    
    * postgres server in the workflow
    
    * postgres server in the workflow
    
    * postgres server in the workflow
    
    * postgres server in the workflow
    
    * removed test from the node workflow
    
    * postgres server in the workflow
    
    * postgres server in the workflow

--- test/app.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest'
-import app from '../src/app'
-
-describe('GET /random-url', () => {
-  it('should render Not Found page', done => {
-    request(app).get('/random-url')
-      .expect(200, done)
-  })
-})
