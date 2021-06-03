c2008ead294cc7354689fe33e5d6da4fb179ba48
János Fehér <34893944+OmTheTurtle@users.noreply.github.com>
2021-06-03 20:38:58 +0200

    Refactor rooms page layout (#699)

--- cypress/integration/darkmode_cookie_test.ts
+++ cypress/integration/darkmode_cookie_test.ts
@@ -1,15 +1,15 @@
 describe('Theme tests', () => {
   it('checks cookies and stylings after theme change', () => {
     cy.visit('/')
-    
+
     cy.get('.cursor-pointer').last().find('svg').click()
     cy.getCookie('theme').should('have.property', 'value', 'dark')
     cy.contains('Tanulószobák').should('css', 'background-color', 'rgb(79, 70, 229)')
-    cy.contains('Foglalás').should('css', 'background-color', 'rgb(167, 139, 250)')  
+    cy.contains('Foglalás').should('css', 'background-color', 'rgb(167, 139, 250)')
 
     cy.get('.cursor-pointer').last().find('svg').click()
     cy.getCookie('theme').should('have.property', 'value', 'light')
-    cy.contains('Tanulószobák').should('css', 'background-color', 'rgb(249, 250, 251)') 
-    cy.contains('Foglalás').should('css', 'background-color', 'rgb(253, 186, 116)') 
+    cy.contains('Tanulószobák').should('css', 'background-color', 'rgb(249, 250, 251)')
+    cy.contains('Foglalás').should('css', 'background-color', 'rgb(254, 215, 170)')
   })
-})  
+})
