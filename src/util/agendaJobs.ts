1eb5dd5a6246663d85956848df75179a3be9ac66
Tschonti <feketesamu@gmail.com>
2021-07-01 12:16:57 +0200

    docker v2

--- src/util/agendaJobs.ts
+++ src/util/agendaJobs.ts
@@ -1,7 +1,7 @@
 import { sendEmail } from '../util/sendEmail'
 import { Agenda } from 'agenda/es'
 
-export const agenda = new Agenda({ db: { address: process.env.MONGO_CONNECTION } })
+export const agenda = new Agenda({ db: { address: 'mongodb://127.0.0.1:27017' } })
 
 agenda.define('send email', async (job) => {
   await sendEmail(job.attrs.data.users, job.attrs.data.email)
