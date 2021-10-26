9e12c9e00831b7edb0b5345662da132ef1de2bc9
Tschonti <feketesamu@gmail.com>
2021-10-26 21:29:19 +0200

    fixed cypress test

--- cypress/integration/darkmode_cookie_test.ts
+++ cypress/integration/darkmode_cookie_test.ts
@@ -2,12 +2,12 @@ describe('Theme tests', () => {
   it('checks cookies and stylings after theme change', () => {
     cy.visit('/')
 
-    cy.get('.cursor-pointer').last().find('svg').click()
+    cy.get('.moonIcon').last().click()
     cy.getCookie('theme').should('have.property', 'value', 'dark')
     cy.contains('Tanulószobák').should('css', 'background-color', 'rgb(79, 70, 229)')
     cy.contains('Foglalás').should('css', 'background-color', 'rgb(167, 139, 250)')
 
-    cy.get('.cursor-pointer').last().find('svg').click()
+    cy.get('.sunIcon').last().click()
     cy.getCookie('theme').should('have.property', 'value', 'light')
     cy.contains('Tanulószobák').should('css', 'background-color', 'rgb(249, 250, 251)')
     cy.contains('Foglalás').should('css', 'background-color', 'rgb(254, 215, 170)')
